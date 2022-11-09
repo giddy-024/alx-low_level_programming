8b9c00081abfb6cdf042b51b08275201bf3a3b8a
Gideon Oppong <goppong010@gmail.com>
2022-11-09 19:43:39 +0000

    Quick fix of file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 
 	while (*(s1 + len1) != '\0')
 		len1++;
